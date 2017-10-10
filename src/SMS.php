baefe7f4612e50babae73547ca84d22c3ec6acc4
JC <juanpinillos@gmail.com>
2017-10-10 13:29:51 -0700

    Revert "Attempt to add alias"
    This reverts commit 8fa7bbc3c48109b7ab0ae836d094f2e7a69bf306.

--- src/SMS.php
+++ src/SMS.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace maissoftware\sms;
-use Yii;
 
 /**
  * sms module definition class
@@ -27,8 +26,8 @@ class SMS extends \yii\base\Module
     public function init()
     {
         parent::init();
+
         // custom initialization code goes here
-        Yii::setAlias("@maissoftware/sms", __DIR__);
     }
 
     public function getSid(){
